13798f9c24222dae483ea337cad702f3e4301e7c
Emily <emilykwong@Emilys-MacBook-Pro.local>
2025-08-18 17:38:47 -0500

    about

--- src/About.jsx
+++ src/About.jsx
@@ -6,7 +6,7 @@ function About() {
       <div className="tool-placeholder">
         <h2>📖 About Type A Friend</h2>
         <p>Hi, I'm Em. This is a goofy site I build in order to make planning with my friends easier. The hardest part is always finding a time that works for everyone, so I figured I can just make my own tool and share it with you guys too.</p>
-        <p>This site is completely free to use, but I have ads just to help me out 🥺.</p>
+        <p>This site is completely free to use, but I have ads just to help me out 🥺. Give bro a chance to survive in this economy.</p>
 
         <Link to="/">← Back to Home</Link>
       </div>
