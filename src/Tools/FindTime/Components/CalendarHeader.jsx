c63f8e09332fb5687fdd7ca6cc85153c6cfcdb79
Emily <emilykwong@Emilys-MacBook-Pro.local>
2025-08-22 14:53:38 -0500

    leaf button but its kinda too small

--- src/Tools/FindTime/Components/CalendarHeader.jsx
+++ src/Tools/FindTime/Components/CalendarHeader.jsx
@@ -1,5 +1,7 @@
 
 import './CalendarHeader.css';
+import rightLeaf from '../../../assets/leaf5.svg';
+import leftLeaf from '../../../assets/leaf6.svg';
 
 export default function CalendarHeader({
 	displayYear,
@@ -12,38 +14,44 @@ export default function CalendarHeader({
 	setDisplayYear,
 	setDisplayMonth
 }) {
-	return (
-			<div className="calendar-header-nav">
-				<button
-					type="button"
-					className="calendar-nav-btn styled-nav-btn"
-					onClick={goToPrevMonth}
-					disabled={displayYear === minDate.getFullYear() && displayMonth === minDate.getMonth()}
-					aria-label="Previous Month"
-				>
-					&#8592; Prev
-				</button>
-				<button
-					type="button"
-					className="calendar-nav-btn styled-nav-btn today-btn"
-					onClick={() => {
-						setDisplayYear(today.getFullYear());
-						setDisplayMonth(today.getMonth());
-					}}
-					aria-label="Go to Today"
-					disabled={displayYear === today.getFullYear() && displayMonth === today.getMonth()}
-				>
-					Today
-				</button>
-				<button
-					type="button"
-					className="calendar-nav-btn styled-nav-btn"
-					onClick={goToNextMonth}
-					disabled={displayYear === maxDate.getFullYear() && displayMonth === maxDate.getMonth()}
-					aria-label="Next Month"
-				>
-					Next &#8594;
-				</button>
-			</div>
+		return (
+					<div className="calendar-header-nav">
+						<div className="calendar-header-col left">
+									<button
+										type="button"
+										className="leaf-nav-btn"
+										aria-label="Previous Month"
+										onClick={goToPrevMonth}
+										disabled={displayYear === minDate.getFullYear() && displayMonth === minDate.getMonth()}
+									>	
+                                    <img src={leftLeaf} alt="" width={28} height={28} />
+									</button>
+						</div>
+						<div className="calendar-header-col center">
+							<button
+								type="button"
+								className="calendar-nav-btn styled-nav-btn today-btn"
+								onClick={() => {
+									setDisplayYear(today.getFullYear());
+									setDisplayMonth(today.getMonth());
+								}}
+								aria-label="Go to Today"
+								disabled={displayYear === today.getFullYear() && displayMonth === today.getMonth()}
+							>
+								Today
+							</button>
+						</div>
+						<div className="calendar-header-col right">
+									<button
+										type="button"
+										className="leaf-nav-btn"
+										aria-label="Next Month"
+										onClick={goToNextMonth}
+										disabled={displayYear === maxDate.getFullYear() && displayMonth === maxDate.getMonth()}
+									>
+                                    <img src={rightLeaf} alt="" width={28} height={28} />
+									</button>
+						</div>
+					</div>
 	);
 }
